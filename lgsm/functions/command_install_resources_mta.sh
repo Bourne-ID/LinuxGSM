dbdd85a1fc19023eb9476dfd80fee223b2022112
Robin Bourne <cam40902@gmail.com>
2019-06-27 15:06:25 +0100

    Fix Auto Install for MTA

--- lgsm/functions/command_install_resources_mta.sh
+++ lgsm/functions/command_install_resources_mta.sh
@@ -20,6 +20,6 @@ fn_install_resources(){
 fn_print_header
 
 fn_print_warning_nl "Installing the default resources with existing resources may cause issues."
-if fn_prompt_yn "Do you want to install MTA default resources?" Y; then
+if [[ !-z "${autoinstall}" || fn_prompt_yn "Do you want to install MTA default resources?" Y ]]; then
 	fn_install_resources
 fi
